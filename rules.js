aaf3f2be66c3e1e5bfdd01f85e1620aacecda8c9
jp1971 <jameson@athleticsnyc.com>
2016-12-09 00:01:41 -0500

    Adding complete text for R1 and R2.

--- rules.js
+++ rules.js
@@ -3,12 +3,27 @@ Read all of the "R" rules entries before starting the adventure. To get started,
 `
 
 var r1_text = `
+R1. Welcome:
+
 Welcome to the world of Sargon the Wizard. You're about to set out on a thrilling, magic-filled adventure in a dangerous, haunted castle, and you'll be on your own. Only your wits and your weapons can help you.
 
-If you choose...
+If you choose one of the prerolled charac- ters provided with the adventure, your char- acter will be a magic-user or an elf. If you decide to use your own character for this adventure, the character cannot be higher than third level. Your character cannot use any spell, magic item, or equipment that is not listed on p. 31-32.
+
+This is a solo adventure. You read entries just like this one. Each entry asks you to make a decision about where you want to go or what you want to do. Some entries, like this one, simply direct you to the next entry. Read entry R2.
+`
+
+var r2_text = `
+R2. Choosing an entry:
+
+Most entries in this adventure list several options for you. There are many different areas to explore in and around Lion Castle. Because of this, all of the adventure entries are let- tered and numbered. If you are exploring the areas outside the castle, all entries begin with the letter "C" (courtyard entries). If you are exploring inside the castle, all entries begin with the letter "L" (Lion Castle entries).
+
+The "C" entries are divided into five areas: the castle outskirts, the outer curtain, the outer ward, the inner curtain, and the inner ward. The "L" entries encompass sev- eral levels of the castle, but are not divided into specific categories.
+
+For details on keeping records during your adventure, read entry R3.
 `
 
 module.exports = {
   start_text: start_text,
-  r1_text: r1_text
+  r1_text: r1_text,
+  r2_text: r2_text
 }
