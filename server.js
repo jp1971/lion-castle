d0fec89f37d53867951bc4eda101443fc170225e
jp1971 <jameson@athleticsnyc.com>
2016-12-08 22:56:49 -0500

    Adding matcher example.

--- server.js
+++ server.js
@@ -21,12 +21,19 @@ slapp.message('start', ['mention', 'direct_message'], (msg) => {
   msg.say(rules.start_text)
 })
 
-slapp.message('^read <([^>]+)>', ['mention', 'direct_message'], (msg, text, type, entry) => {
+slapp.message('^read .*', ['mention', 'direct_message'], (msg, text, type, entry) => {
   // text = 'play artist spotify:track:1yJiE307EBIzOB9kqH1deb'
   // type = 'artist'
   // toplay = 'spotify:track:1yJiE307EBIzOB9kqH1deb'
   msg.say('You want to read ${entry}')
 })
+
+slapp.message('^play (song|artist) <([^>]+)>', (msg, text, type, toplay) => {
+  // text = 'play artist spotify:track:1yJiE307EBIzOB9kqH1deb'
+  // type = 'artist'
+  // toplay = 'spotify:track:1yJiE307EBIzOB9kqH1deb'
+  msg.say(toplay)
+}
 
 // var HELP_TEXT = `
 // I will respond to the following messages:
