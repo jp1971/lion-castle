f8347892c356c6430617431463c50a21667a4088
jp1971 <jameson@athleticsnyc.com>
2016-12-08 22:48:20 -0500

    Adding typeFilter.

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ slapp.message('start', ['mention', 'direct_message'], (msg) => {
   msg.say(rules.start_text)
 })
 
-slapp.message('^read <([^>]+)>', (msg, text, type, entry) => {
+slapp.message('^read <([^>]+)>', ['mention', 'direct_message'], (msg, text, type, entry) => {
   // text = 'play artist spotify:track:1yJiE307EBIzOB9kqH1deb'
   // type = 'artist'
   // toplay = 'spotify:track:1yJiE307EBIzOB9kqH1deb'
