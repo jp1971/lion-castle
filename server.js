261712c8c1b0a71deb162ea03e9ccefdba68b8ff
jp1971 <jameson@athleticsnyc.com>
2016-12-08 22:14:47 -0500

    Setting proper path for rules.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
 const Context = require('slapp-context-beepboop')
 
-var rules = require('rules')
+var rules = require('./rules')
 
 // use `PORT` env var on Beep Boop - default to 3000 locally
 var port = process.env.PORT || 3000
