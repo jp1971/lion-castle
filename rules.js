a556ecb068ea480afb45dc695fc3b7e6b410eb33
jp1971 <jameson@athleticsnyc.com>
2016-12-08 23:31:34 -0500

    Escaping.

--- rules.js
+++ rules.js
@@ -1,5 +1,13 @@
 var start_text = `
-Read all of the \"R\" rules entries before starting the adventure. To get started, type \`Read R1\`
+Read all of the \"R\" rules entries before starting the adventure. To get started, type \`Read R1\`.
 `
 
-exports.start_text = start_text;
+var r1_text = `
+Welcome to the world of Sargon the Wizard. You\'re about to set out on a thrilling, magic-filled adventure in a dangerous, haunted castle, and you\'ll be on your own. Only your wits and your weapons can help you.
+If you choose...
+`
+
+module.exports = {
+  start_text = start_text,
+  r1_text = r1_text
+}
